ba6ba69dd3ea09412eda8650de3bc197ac78035a
Ericson Monteiro <seuemail@gmail.com>
2026-05-11 21:09:05 -0100

    remove deprecated OAuth 2.0 implementation files and related assets

--- oauthgoogle/googleoauthapp/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$client_id = "gq5rpgffnatbv18eq2lt4o4026dgbsp4";
-$client_secret = "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";
-$redirect_uri = " https://marianna-contradictable-winnie.ngrok-free.dev/gitoauthapp/php/callback.php";
-
-$authorize_url = "https://accounts.google.com/o/oauth2/v2/auth";
-$token_url     = "https://oauth2.googleapis.com/token";
-$user_api      = "https://www.googleapis.com/oauth2/v3/userinfo";
